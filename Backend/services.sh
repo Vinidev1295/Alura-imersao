cfce5e5135e76585476e9bf6f2c2c243588d53de
Vinidev1295 <vinidias1295@gmail.com>
2024-11-24 03:38:44 -0300

    Update services.sh

--- Backend/services.sh
+++ Backend/services.sh
@@ -1,3 +1,4 @@
+gcloud config set project genuine-box-442706-t3
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
